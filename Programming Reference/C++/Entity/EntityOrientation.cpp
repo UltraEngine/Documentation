3b82e364b2a444ef3fb49a32a24dad94b08b550f
Leadwerks Software <support@leadwerks.com>
2020-04-24 08:38:07 +0300

    Update EntityOrientation.cpp

--- Programming Reference/C++/Entity/EntityOrientation.cpp
+++ Programming Reference/C++/Entity/EntityOrientation.cpp
@@ -4,6 +4,11 @@ void main()
   
   auto window = CreateWindow("",0,0,1280,720,nullptr,WINDOW_CENTER | WINDOW_TITLEBAR)
 
+    
+  box->SetPosition(1,0,0);
+  box->SetRotation(0,0,45);
+  box->SetScale(2,1,1);
+    
   while (window->Closed() == false)
   {
     world->Update();
